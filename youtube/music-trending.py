d14f300cad242494b4028c1538b269b40a42968e
khoidd <khoidd@vng.com.vn>
2020-05-28 10:37:49 +0700

    add youtube crawler

---
+++ youtube/music-trending.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+import pandas as pd
+from selenium import webdriver
+from pyvirtualdisplay import Display
+import schedule
+import time, os
+
+def start_chrome():
+    # # Chrome Version
+    CHROME_VERSION = "81"
+    # # Chrom Dirver
+    CHROME_ADDRESS = '../../chromedriver/%s' % (CHROME_VERSION)
+    # # Chrome Options
+    CHROME_OPTIONS = webdriver.ChromeOptions()
+    CHROME_OPTIONS.add_argument("start-maximized")
+    CHROME_OPTIONS.add_argument("--disable-extensions")
+    CHROME_OPTIONS.add_argument("--disable-gpu")
+    CHROME_OPTIONS.add_argument("--no-sandbox")
+    CHROME_OPTIONS.add_argument('disable_infobars')
+    CHROME_OPTIONS.add_argument("--headless")
+    # # Virtual Display
+    display = Display(visible=0, size=(800, 800))
+
+    try:
+        driver = webdriver.Chrome(CHROME_ADDRESS, options=CHROME_OPTIONS)
+        display = Display(visible=0, size=(800, 800)); display.start()
+    except:
+        print("[BUG][CHROME] Can not start Chrome! Check your Xvbf and Chrome version.")
+    
+    return display, driver
+
+def crawl_youtube(driver):
+    # Access web by webdriver (YOUTUBE trending)
+    ORIGIN_LINK = 'https://www.youtube.com/feed/trending'
+    driver.get(ORIGIN_LINK)
+
+    # Find music trending link
+    xpath = '//*[@id="contents"]/ytd-channel-list-sub-menu-avatar-renderer[1]/a'
+    trending_links = driver.find_elements_by_xpath(xpath)[0]
+    driver.get(trending_links.get_attribute('href'))
+
+    # Scroll to load all video
+    driver.execute_script(
+        "window.scrollTo(0, document.documentElement.scrollHeight)")
+
+    # Get artist links
+    xpath = '//div[@id="meta"]/ytd-video-meta-block/div[1]/div[1]/ytd-channel-name/div/div/yt-formatted-string/a'
+    artist_list = driver.find_elements_by_xpath(xpath)
+
+    artist_infos = list()
+    for x in artist_list[:-1]:
+        artist_infos.append(
+            {'name': x.text, "link": x.get_attribute("href")[24:]})
+
+    timestamp = datetime.now().strftime("%d-%b-%Y-%H")
+    df = pd.DataFrame(data=artist_infos)
+    df = df.drop_duplicates(ignore_index=True)
+    df.to_csv('data/artist/artist-%s.csv' % (timestamp))
+
+    # Get video links
+    xpath = '//a[@id="video-title"]'
+    video_list = driver.find_elements_by_xpath(xpath)
+
+    video_infos = list()
+    for x in video_list[:-1]:
+        video_infos.append({'title': x.get_attribute("title"),
+                            "link": x.get_attribute("href")[32:]})
+
+    timestamp = datetime.now().strftime("%d-%b-%Y-%H")
+    df = pd.DataFrame(data=video_infos)
+    df = df.drop_duplicates(ignore_index=True)
+    df.to_csv('data/video/video-%s.csv' % (timestamp))
+
+def job():
+    try:
+        display, driver = start_chrome()
+        crawl_youtube(driver)
+        print('Downloaded at ' + datetime.now().strftime("%d-%b-%Y-%H"))
+    finally:
+        driver.close()
+        display.stop()
+
+if __name__ == "__main__":
+    job()
+    schedule.every().hour.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
---
+++ youtube/video-channel.py
@@ -0,0 +1,91 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import youtube_dl
+import argparse
+from os import listdir, mkdir
+import time
+
+# Add the arguments to the parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-l", "--key", type=str, nargs=2, required=True)
+ap.add_argument("-s", "--scroll", type=int, nargs=2, default=[30, 1])
+ap.add_argument("-t", "--time", type=int, nargs=2, default=[120, 600])
+
+
+args = vars(ap.parse_args())
+TYPE, KEY = args['key']
+SCROLL, TIME = args['scroll']
+MIN, MAX = args['time']
+
+# Setup Chrome Webdriver
+CHROME_VERSION = "80"
+CHROME_ADDRESS = '../../../chromedriver/%s'
+options = webdriver.ChromeOptions()
+options.add_argument('disable_infobars')
+driver = webdriver.Chrome(
+    CHROME_ADDRESS % (CHROME_VERSION),
+    options=options)
+waiter = WebDriverWait(driver, 10)
+
+# Access web by webdriver (YOUTUBE trending)
+ORIGIN_LINK = 'https://www.youtube.com/'
+if TYPE == 'search':
+    ORIGIN_LINK += 'results?search_query=%s' 
+elif TYPE == 'channel':
+    ORIGIN_LINK += 'channel/%s/videos'
+driver.get(ORIGIN_LINK % (KEY))
+
+for _ in range(SCROLL):
+    time.sleep(TIME)
+    driver.execute_script(
+        "window.scrollTo(0, document.documentElement.scrollHeight)")
+
+# Extract link list of trending videos
+video_list = []
+if TYPE == 'search':
+    video_list = driver.find_elements_by_xpath('//a[@id="video-title"]')
+elif TYPE == 'channel':
+    video_list = driver.find_elements_by_xpath('//a[@id="thumbnail"]')
+
+video_links = [x.get_attribute('href') for x in video_list]
+video_links = [x[32:] for x in video_links if x != None]
+
+exist_links = []
+try:
+    exist_links = [f[:-4] for f in listdir("../data")]
+except FileNotFoundError:
+    print("[WARNING] Creat data directory")
+    mkdir('../data')
+
+video_links = [x for x in video_links if x not in exist_links]
+
+# Close web driver
+driver.close()
+
+# https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/YoutubeDL.py
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'data/%(id)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+# download audio from youtube
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    for link in video_links:
+        link = 'https://www.youtube.com/watch?v=' + link
+
+        try:
+            info = ydl.extract_info(link, download=False)
+        except:
+            print("[WARNING] Sign in to confirm your age.")
+            continue
+        
+        duration = info['duration']
+        if duration < MAX and duration > MIN:
+            ydl.download([link])
+        else:
+            print('[WARNING] Duration is out of limit: %i', duration)
